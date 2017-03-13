3661236d3a5318449dbb549009b1414ff6bac525
Randy Davis <randy_davis@outlook.com>
2017-03-12 22:44:55 -0500

    complete 'Exploring State and Events - Selecting Items'

--- 04_StateAndEvents/dropdown/src/List-Item.jsx
+++ 04_StateAndEvents/dropdown/src/List-Item.jsx
@@ -1,9 +1,12 @@
 var React = require('react');
 
 var ListItem = React.createClass({
+    handleClick: function () {
+        this.props.whenItemClicked(this.props.item);
+    },
     render: function () {
         return(
-            <li><a href="#">{this.props.item}</a></li>
+            <li className={this.props.className}><a href="#" onClick={this.handleClick}>{this.props.item}</a></li>
         )
     }
 });
